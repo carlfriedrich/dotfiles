6b4565c52c0b692d8c31a2cd2879940957cfeef8
carlfriedrich <carlfriedrich@posteo.de>
2022-02-23 15:11:40 +0100

    git-fuzzy-log: refactoring
    Move variables to function so that they do not pollute environment. Had
    to define GIT_FL_KEYBINDINGS as an array in order to have well readable
    syntax.

--- bash_functions.d/git-fuzzy-log.sh
+++ bash_functions.d/git-fuzzy-log.sh
@@ -4,24 +4,30 @@
 # Provide an interactive git log using fzf. Based on:
 # https://gist.github.com/junegunn/f4fca918e937e6bf5bad?permalink_comment_id=2731105#gistcomment-2731105
 #-------------------------------------------------------------------------------
-GIT_FL_ENTER_COMMAND="(grep -o '[a-f0-9]\{7\}' | head -1 |
-	xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less --tabs=4 -R') << 'FZF-EOF'
-	{}
-FZF-EOF"
-GIT_FL_PREVIEW_COMMAND='f() { \
-	set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); \
-	[ $# -eq 0 ] || git show --no-patch --color=always $1 ; \
-}; f {}'
-GIT_FL_KEYBINDINGS="\
-shift-down:preview-down,\
-shift-up:preview-up,\
-pgdn:preview-page-down,\
-pgup:preview-page-up,\
-q:abort,\
-enter:execute:${GIT_FL_ENTER_COMMAND}\
-"
 git-fuzzy-log ()
 {
+	GIT_FL_PREVIEW_COMMAND='f() {
+		set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}")
+		[ $# -eq 0 ] || git show --no-patch --color=always $1
+	}; f {}'
+
+	GIT_FL_ENTER_COMMAND='(grep -o "[a-f0-9]\{7\}" | head -1 |
+		xargs -I % sh -c "git show --color=always % | diff-so-fancy | 
+		less --tabs=4 -R") << "FZF-EOF"
+		{}
+		FZF-EOF'
+
+	# Define keybindings in array and then convert it to a comma-separated list
+	GIT_FL_KEYBINDINGS=(
+		"shift-down:preview-down"
+		"shift-up:preview-up"
+		"pgdn:preview-page-down"
+		"pgup:preview-page-up"
+		"q:abort"
+		"enter:execute:${GIT_FL_ENTER_COMMAND}"
+	)
+	GIT_FL_KEYBINDINGS=$(IFS=','; echo "${GIT_FL_KEYBINDINGS[*]}")
+
 	git log --graph --color=always --format="%C(auto)%h %s%d " | \
 		fzf --ansi --no-sort --reverse --tiebreak=index \
 		--preview "${GIT_FL_PREVIEW_COMMAND}" --preview-window=top:15 \
