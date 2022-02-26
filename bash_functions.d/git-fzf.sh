19ced9772c1817cd74020fcef0a912c4f450ef90
carlfriedrich <carlfriedrich@posteo.de>
2022-02-26 19:52:47 +0100

    git-fzf: simplified variable names
    Since the variables have local scope within their functions, no need to
    add a prefix to them.

--- bash_functions.d/git-fzf.sh
+++ bash_functions.d/git-fzf.sh
@@ -7,43 +7,43 @@
 #-------------------------------------------------------------------------------
 git-fuzzy-diff ()
 {
-	GIT_FD_PREVIEW_PAGER="less --tabs=4 -Rc"
-	GIT_FD_ENTER_PAGER=${GIT_FD_PREVIEW_PAGER}
+	PREVIEW_PAGER="less --tabs=4 -Rc"
+	ENTER_PAGER=${PREVIEW_PAGER}
 	if [ -x "$(command -v diff-so-fancy)" ]; then
-		GIT_FD_PREVIEW_PAGER="diff-so-fancy | ${GIT_FD_PREVIEW_PAGER}"
-		GIT_FD_ENTER_PAGER="diff-so-fancy | sed -e '1,4d' | ${GIT_FD_ENTER_PAGER}"
+		PREVIEW_PAGER="diff-so-fancy | ${PREVIEW_PAGER}"
+		ENTER_PAGER="diff-so-fancy | sed -e '1,4d' | ${ENTER_PAGER}"
 	fi
 
 	# Don't just diff the selected file alone, get related files first using
 	# '--name-status -R' in order to include moves and renames in the diff.
 	# See for reference: https://stackoverflow.com/q/71268388/3018229
-	GIT_FD_PREVIEW_COMMAND='git diff --color=always '$@' -- \
+	PREVIEW_COMMAND='git diff --color=always '$@' -- \
 		$(echo $(git diff --name-status -R '$@' | grep {}) | cut -d" " -f 2-) \
-		| '$GIT_FD_PREVIEW_PAGER
+		| '$PREVIEW_PAGER
 
 	# Show additional context compared to preview
-	GIT_FD_ENTER_COMMAND='git diff --color=always '$@' -U10000 -- \
+	ENTER_COMMAND='git diff --color=always '$@' -U10000 -- \
 		$(echo $(git diff --name-status -R '$@' | grep {}) | cut -d" " -f 2-) \
-		| '$GIT_FD_ENTER_PAGER
+		| '$ENTER_PAGER
 
-	GIT_FD_KEYBINDINGS=(
+	KEYBINDINGS=(
 		"shift-down:preview-down"
 		"shift-up:preview-up"
 		"pgdn:preview-page-down"
 		"pgup:preview-page-up"
 		"q:abort"
-		"enter:execute:${GIT_FD_ENTER_COMMAND}"
+		"enter:execute:${ENTER_COMMAND}"
 	)
-	GIT_FD_KEYBINDINGS=$(IFS=','; echo "${GIT_FD_KEYBINDINGS[*]}")
+	KEYBINDINGS=$(IFS=','; echo "${KEYBINDINGS[*]}")
 
 	git diff --name-only $@ | \
-		fzf --ansi --reverse --exit-0 --preview "${GIT_FD_PREVIEW_COMMAND}" \
-		--preview-window=top:85% --bind "${GIT_FD_KEYBINDINGS}"
+		fzf --ansi --reverse --exit-0 --preview "${PREVIEW_COMMAND}" \
+		--preview-window=top:85% --bind "${KEYBINDINGS}"
 }
 
 git-fuzzy-log ()
 {
-	GIT_FL_PREVIEW_COMMAND='f() {
+	PREVIEW_COMMAND='f() {
 		set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}")
 		[ $# -eq 0 ] || (
 			git show --no-patch --color=always $1
@@ -58,23 +58,23 @@ git-fuzzy-log ()
 		)
 	}; f {}'
 
-	GIT_FL_ENTER_COMMAND='(grep -o "[a-f0-9]\{7\}" | head -1 |
+	ENTER_COMMAND='(grep -o "[a-f0-9]\{7\}" | head -1 |
 		xargs -I % bash -ic "git-fuzzy-diff %^1 %") <<- "FZF-EOF"
 		{}
 		FZF-EOF'
 
-	GIT_FL_KEYBINDINGS=(
+	KEYBINDINGS=(
 		"shift-down:preview-down"
 		"shift-up:preview-up"
 		"pgdn:preview-page-down"
 		"pgup:preview-page-up"
 		"q:abort"
-		"enter:execute:${GIT_FL_ENTER_COMMAND}"
+		"enter:execute:${ENTER_COMMAND}"
 	)
-	GIT_FL_KEYBINDINGS=$(IFS=','; echo "${GIT_FL_KEYBINDINGS[*]}")
+	KEYBINDINGS=$(IFS=','; echo "${KEYBINDINGS[*]}")
 
 	git log --graph --color=always --format="%C(auto)%h %s%d " | \
 		fzf --ansi --no-sort --reverse --tiebreak=index \
-		--preview "${GIT_FL_PREVIEW_COMMAND}" --preview-window=top:15 \
-		--bind "${GIT_FL_KEYBINDINGS}"
+		--preview "${PREVIEW_COMMAND}" --preview-window=top:15 \
+		--bind "${KEYBINDINGS}"
 }
