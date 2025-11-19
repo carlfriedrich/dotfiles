435cc122f476831970e0e3be3339903203497900
carlfriedrich <carlfriedrich@posteo.de>
2025-11-19 10:51:01 +0100

    rg-find: add ctrl-h keybinding to toggle hidden files

--- bash_functions.d/rg-find.sh
+++ bash_functions.d/rg-find.sh
@@ -8,14 +8,21 @@
 
 __rg_find() {
     RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+    FZF_RELOAD_COMMAND="reload:sleep 0.1; $RG_PREFIX {q} || true"
+    FZF_RELOAD_COMMAND_HIDDEN="reload:sleep 0.1; $RG_PREFIX --hidden {q} || true"
     INITIAL_QUERY="${*:-}"
     IFS=: read -ra selected < <(
       FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
       fzf --ansi \
           --disabled --query "$INITIAL_QUERY" \
-          --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+          --bind 'change:transform:[[ $FZF_PROMPT =~ \+hidden ]] && \
+              echo "'"$FZF_RELOAD_COMMAND_HIDDEN"'" || \
+              echo "'"$FZF_RELOAD_COMMAND"'"' \
           --bind "alt-enter:execute-silent($EDITOR -g '{1}:{2}:{3}')" \
           --bind "tab:toggle-preview" \
+          --bind 'ctrl-h:transform:[[ ! $FZF_PROMPT =~ \+hidden ]] && \
+              echo "change-prompt(+hidden> )+reload('"$FZF_RELOAD_COMMAND_HIDDEN"')" || \
+              echo "change-prompt(> )+reload('"$FZF_RELOAD_COMMAND"')"' \
           --delimiter : \
           --preview 'bat --color=always {1} --style=numbers --highlight-line {2}' \
           --preview-window 'up,50%,+{2}/2:hidden' \
